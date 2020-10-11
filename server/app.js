e6425ee332027472251985538b792b2d3ed42c20
Gginne <ggamerstar@gmail.com>
2020-10-11 03:28:32 -0400

    routing

--- server/app.js
+++ server/app.js
@@ -14,8 +14,8 @@ app.set('port', process.env.PORT || 5000)
 
 //Middleware
 app.use(express.json())
-app.use(express.static(path.join(__dirname, 'client/build')))
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/../client/build')))
+app.use(express.static(path.join(__dirname, '/../public')));
 app.use(express.urlencoded({extended: false}))
 app.use(cors())
 
@@ -25,7 +25,7 @@ app.use("/api/photos", photoRoutes)
 app.use("/api/albums", albumRoutes)
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + 'client/build/index.html'))
+    res.sendFile(path.join(__dirname + '/../client/build/index.html'))
 })
 
 
