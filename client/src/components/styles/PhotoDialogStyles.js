18b2c5abf067ef3e330cdc21d423554107b72d66
Gginne <23042760+Gginne@users.noreply.github.com>
2021-01-06 17:17:12 -0400

    created show more mechanism for photo dialof

--- client/src/components/styles/PhotoDialogStyles.js
+++ client/src/components/styles/PhotoDialogStyles.js
@@ -11,10 +11,16 @@ const styles = (theme) => ({
   },
   dialogImg: {
     maxWidth: "70vw",
-    minWidth: "23vw",
+    minWidth: "20vw",
     height: "auto",
     maxHeight: "60vh",
     margin: "auto"
+  },
+  buttons: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: theme.spacing(1),
+    color: theme.palette.grey[500],
   },
   paper: {
     maxHeight: '80vh',
